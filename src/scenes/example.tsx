7c62a849e5c3e7b2bba7c6b9aad02ea188d1b901
Alexander_Ovchar <azalexxx02@gmail.com>
2024-11-30 19:02:54 +0200

    add first rect

--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -1,12 +1,32 @@
-import {Circle, makeScene2D} from '@motion-canvas/2d';
-import {createRef} from '@motion-canvas/core';
+import {Circle, Icon, Layout, makeScene2D, Rect, Txt} from "@motion-canvas/2d";
+import {createRef} from "@motion-canvas/core";
+import {IoIosNotifications} from "react-icons/io";
 
 export default makeScene2D(function* (view) {
-  // Create your animations here
+  // // Create your animations here
 
-  const circle = createRef<Circle>();
+  // const circle = createRef<Circle>();
 
-  view.add(<Circle ref={circle} size={320} fill={'lightseagreen'} />);
+  // view.add(<Circle ref={circle} size={320} fill={'lightseagreen'} />);
 
-  yield* circle().scale(2, 2).to(1, 2);
+  // yield* circle().scale(2, 2).to(1, 2);
+
+  view.add(
+    <Rect fill="#525B44" layout direction="column" radius={10} padding={10}>
+      <Layout gap={10} justifyContent="space-between">
+        <Icon icon={"material-symbols:notifications-rounded"} color="white" size={20} />
+        <Layout gap={10}>
+          <Circle size={20} fill="red" />
+          <Circle size={20} fill="yellow" />
+          <Circle size={20} fill="green" />
+        </Layout>
+      </Layout>
+      <Txt
+        text="I love JavaScript"
+        fill="white"
+        fontSize={20}
+        fontFamily="Arial"
+      />
+    </Rect>
+  );
 });
