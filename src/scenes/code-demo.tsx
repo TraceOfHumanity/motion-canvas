ea8ceaaffc3c0ca8b6043b39bff7d7c0dc663243
Alexander_Ovchar <azalexxx02@gmail.com>
2024-12-01 00:15:40 +0200

    update number array

--- src/scenes/code-demo.tsx
+++ src/scenes/code-demo.tsx
@@ -63,8 +63,8 @@ export default makeScene2D(function* (view) {
   yield* codeRef().selection(word(1, 16, 8), 0.5);
   yield* waitFor(1);
   for (let i = 0; i < 15; i += 3) {
-    yield* codeRef().selection(word(0, 21 + i, 1), 0.5);
-    yield* waitFor(0.2);
+    yield* codeRef().selection(word(0, 21 + i, 1), 0.3);
+    // yield* waitFor(0.001);
   }
   // yield* codeRef().selection( DEFAULT, 0.6);
   yield* waitFor(0.5);
@@ -88,5 +88,66 @@ export default makeScene2D(function* (view) {
   );
   yield* waitFor(1);
 
-  yield* codeRef().selection(codeRef().findAllRanges(/number/gi), 0.6);
+  yield* codeRef().selection(codeRef().findAllRanges(/\bnumber\b/gi), 0.6);
+  yield* waitFor(1);
+
+  yield* waitFor(1);
+
+  yield* codeRef().selection(DEFAULT, 0.6);
+  yield* all(
+    ...codeRef()
+      .findAllRanges(/\bnumber\b/gi)
+      .map((range) => codeRef().code.replace(range, "1", 0.6))
+  );
+  yield* waitFor(1);
+
+  yield* codeRef().code.edit(0.5)`\
+const numberArray = [1, 2, 3, 4, 5];
+numberArray.map((1) => {
+    return 1 * 2;
+}); // [2]
+  `;
+  yield* waitFor(1);
+
+  yield* codeRef().code.edit(0.5)`\
+const numberArray = [1, 2, 3, 4, 5];
+numberArray.map((2) => {
+    return 2 * 2;
+}); // [2]
+    `;
+  yield* waitFor(1);
+
+    yield* codeRef().code.edit(0.5)`\
+const numberArray = [1, 2, 3, 4, 5];
+numberArray.map((2) => {
+    return 2 * 2;
+}); // [2, 4]
+    `;
+  yield* waitFor(1);
+
+      yield* codeRef().code.edit(0.5)`\
+const numberArray = [1, 2, 3, 4, 5];
+numberArray.map((3) => {
+    return 3 * 2;
+}); // [2, 4, 6]
+    `;
+  yield* waitFor(1);
+
+        yield* codeRef().code.edit(0.5)`\
+const numberArray = [1, 2, 3, 4, 5];
+numberArray.map((4) => {
+    return 4 * 2;
+}); // [2, 4, 6, 8]
+    `;
+  yield* waitFor(1);
+
+    yield* codeRef().code.edit(0.5)`\
+const numberArray = [1, 2, 3, 4, 5];
+numberArray.map((5) => {
+    return 5 * 2;
+}); // [2, 4, 6, 8, 10]
+    `;
+  yield* waitFor(1);
+
+
 });
