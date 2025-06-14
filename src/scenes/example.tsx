da44efb51ea3686627c040613df3d5c5dcb53ba2
Alexander_Ovchar <azalexxx02@gmail.com>
2025-06-14 17:53:54 +0300

    chore(config): add Prettier configuration and update package dependencies

--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -1,12 +1,24 @@
-import {Circle, makeScene2D} from '@motion-canvas/2d';
-import {createRef} from '@motion-canvas/core';
+import { Circle, Layout, makeScene2D, Rect, Txt } from "@motion-canvas/2d";
+import { createRef } from "@motion-canvas/core";
 
 export default makeScene2D(function* (view) {
-  // Create your animations here
+  // // Create your animations here
 
-  const circle = createRef<Circle>();
+  // const circle = createRef<Circle>();
 
-  view.add(<Circle ref={circle} size={320} fill={'lightseagreen'} />);
+  // view.add(<Circle ref={circle} size={320} fill={'lightseagreen'} />);
 
-  yield* circle().scale(2, 2).to(1, 2);
+  // yield* circle().scale(2, 2).to(1, 2);
+
+  view.add(
+    <Rect layout fill="black" padding={40} radius={20} direction="column">
+      <Layout direction="row" gap={10} alignItems="center">
+        <Txt text="title" fontSize={100} fill="white" />
+        <Circle size={100} fill="white" />
+        <Circle size={100} fill="white" />
+        <Circle size={100} fill="white" />
+      </Layout>
+      <Txt text="Hello, world!" fontSize={100} fill="white" />
+    </Rect>,
+  );
 });
